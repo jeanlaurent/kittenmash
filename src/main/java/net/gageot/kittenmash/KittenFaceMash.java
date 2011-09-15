f9f26d6ff2074ed3c920e1d5bd1bab07bf38a696
David Gageot <david@gageot.net>
2011-09-15 18:32:42 +0200

    routes

--- src/main/java/net/gageot/kittenmash/KittenFaceMash.java
+++ src/main/java/net/gageot/kittenmash/KittenFaceMash.java
@@ -3,6 +3,7 @@ package net.gageot.kittenmash;
 import static com.google.common.collect.Iterables.*;
 import static com.google.common.collect.Lists.*;
 import static com.google.inject.Guice.*;
+import static com.google.inject.name.Names.*;
 import static net.gageot.test.Reflection.*;
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -12,7 +13,7 @@ import org.simpleframework.http.core.Container;
 import org.simpleframework.transport.connect.SocketConnection;
 import com.google.common.collect.ImmutableList;
 import com.google.common.util.concurrent.AbstractService;
-import com.google.inject.Injector;
+import com.google.inject.*;
 
 public class KittenFaceMash extends AbstractService implements Container {
 	private SocketConnection socketConnection;
@@ -21,25 +22,23 @@ public class KittenFaceMash extends AbstractService implements Container {
 
 	public KittenFaceMash(int port) {
 		this.port = port;
-		injector = createInjector();
+		injector = createInjector(new AbstractModule() {
+			@Override
+			protected void configure() {
+				bind(Key.get(Object.class, named("index"))).to(IndexController.class);
+				bind(Key.get(Object.class, named("kitten"))).to(KittenController.class);
+				bind(Key.get(Object.class, named("vote"))).to(VoteController.class);
+			}
+		});
 	}
 
 	@Override
 	public void handle(Request req, Response resp) {
-		List<String> path = newArrayList(req.getPath().getSegments());
-		String action = getFirst(path, "index");
-
 		try {
-			Object controller;
-			if ("kitten".equals(action)) {
-				controller = injector.getInstance(KittenController.class);
-			} else if ("vote".equals(action)) {
-				controller = injector.getInstance(VoteController.class);
-			} else {
-				controller = injector.getInstance(IndexController.class);
-			}
+			List<String> path = newArrayList(req.getPath().getSegments());
+			String action = getFirst(path, "index");
 
-			invoke(controller, "render", ImmutableList.builder().add(resp).addAll(skip(path, 1)).build());
+			invoke(controllerForAction(action), "render", arguments(resp, path));
 		} finally {
 			try {
 				resp.close();
@@ -47,6 +46,14 @@ public class KittenFaceMash extends AbstractService implements Container {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	public ImmutableList<Object> arguments(Response resp, List<String> path) {
+		return ImmutableList.builder().add(resp).addAll(skip(path, 1)).build();
+	}
+
+	public Object controllerForAction(String action) {
+		return injector.getInstance(Key.get(Object.class, named(action)));
 	}
 
 	public static void main(String[] args) {
