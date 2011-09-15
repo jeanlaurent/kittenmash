c62c419781ae17ac952d551b78826c09801151fb
David Gageot <david@gageot.net>
2011-09-15 17:51:09 +0200

    run method

--- src/main/java/net/gageot/kittenmash/HelloWorld.java
+++ src/main/java/net/gageot/kittenmash/HelloWorld.java
@@ -6,17 +6,29 @@ import org.simpleframework.http.*;
 import org.simpleframework.http.core.Container;
 import org.simpleframework.transport.connect.SocketConnection;
 
-public class HelloWorld {
+public class HelloWorld implements Container {
+	private SocketConnection socketConnection;
+	private final int port;
+
+	public HelloWorld(int port) {
+		this.port = port;
+	}
+
+	@Override
+	public void handle(Request req, Response resp) {
+		try {
+			resp.getPrintStream().append("hello world").close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) throws Exception {
-		new SocketConnection(new Container() {
-			@Override
-			public void handle(Request req, Response resp) {
-				try {
-					resp.getPrintStream().append("hello world").close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}).connect(new InetSocketAddress(8080));
+		new HelloWorld(8080).run();
+	}
+
+	public void run() throws IOException {
+		socketConnection = new SocketConnection(this);
+		socketConnection.connect(new InetSocketAddress(port));
 	}
 }
