b190249007ac8dc56ae6c3c4ea07e8efa47966b3
David Gageot <david@gageot.net>
2011-09-15 17:49:20 +0200

    Hello World Http style

--- src/main/java/net/gageot/kittenmash/HelloWorld.java
+++ src/main/java/net/gageot/kittenmash/HelloWorld.java
@@ -1,7 +1,26 @@
 package net.gageot.kittenmash;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import org.simpleframework.http.*;
+import org.simpleframework.http.core.Container;
+import org.simpleframework.transport.connect.SocketConnection;
+
 public class HelloWorld {
 	public String sayHello() {
 		return "hello world";
+	}
+
+	public static void main(String[] args) throws Exception {
+		new SocketConnection(new Container() {
+			@Override
+			public void handle(Request req, Response resp) {
+				try {
+					resp.getPrintStream().append("hello world").close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}).connect(new InetSocketAddress(8080));
 	}
 }
